ce2be63f9e985efbc177794b4d2eb28bb2b4e4c3
SunAhong1993 <48579383+SunAhong1993@users.noreply.github.com>
2019-08-20 19:04:05 +0800

    Update labelme2coco.py

--- DataAnnotation/labelme2coco.py
+++ DataAnnotation/labelme2coco.py
@@ -23,11 +23,6 @@ class MyEncoder(json.JSONEncoder):
             return super(MyEncoder, self).default(obj)
 
 
-def getbbox(self, points):
-    polygons = points
-    mask = self.polygons_to_mask([self.height, self.width], polygons)
-    return self.mask2box(mask)
-
 
 def images(data, num):
     image = {}
@@ -69,17 +64,29 @@ def annotations_rectangle(iscrowd, points, label, num, label_to_num, count):
     return annotation
 
 
-def annotations_polygon(iscrowd, height, width, points, label, num,
+def annotations_polygon(annotation, iscrowd, height, width, points, label, num,
                         label_to_num, count):
-    annotation = {}
-    annotation['segmentation'] = [list(np.asarray(points).flatten())]
-    annotation['iscrowd'] = iscrowd
-    annotation['image_id'] = num + 1
-    annotation['bbox'] = list(map(float, get_bbox(height, width, points)))
-    annotation['area'] = annotation['bbox'][2] * annotation['bbox'][3]
-    annotation['category_id'] = label_to_num[label]
-    annotation['id'] = count
-    return annotation
+    
+    if len(annotation) == 0:
+        annotation['segmentation'] = [list(np.asarray(points).flatten())]
+        annotation['iscrowd'] = iscrowd
+        annotation['image_id'] = num + 1
+        annotation['bbox'] = list(map(float, get_bbox(height, width, points)))
+        annotation['area'] = annotation['bbox'][2] * annotation['bbox'][3]
+        annotation['category_id'] = label_to_num[label]
+        annotation['id'] = count
+    else:
+        annotation['segmentation'].append(list(np.asarray(points).flatten()))
+        box1 = annotation['bbox']
+        box2 = list(map(float, get_bbox(height, width, points)))
+        x11, y11, x12, y12 = box1[0], box1[1], box1[0] + box1[2], box1[1] + box1[3]
+        x21, y21, x22, y22 = box2[0], box2[1], box2[0] + box2[2], box2[1] + box2[3]
+        x1 = x21 if x11 > x21 else x11
+        y1 = y21 if y11 > y21 else y11
+        x2 = x22 if x12 < x22 else x12
+        y2 = y22 if y12 < y22 else y12
+        annotation['bbox'] = [x1, y1, x2 - x1, y2 - y1]
+
 
 
 def get_bbox(height, width, points):
@@ -117,17 +124,21 @@ def deal_json(img_path, json_path):
         if img_label == '':
             continue
         label_file = osp.join(json_path, img_label + '.json')
+        assert os.path.exists(label_file), \
+            'The .json file of {} is not exists!'.format(img_file)
         print('Generating dataset from:', label_file)
         num = num + 1
         with open(label_file) as f:
             data = json.load(f)
             images_list.append(images(data, num))
             count = 0
+            lmid_count = {}
             for shapes in data['shapes']:
                 count += 1
                 label = shapes['label']
-                iscrowd = int(label.split('_')[-1])
-                label = label[:-2]
+                part = label.split('_')
+                iscrowd = int(part[-1][0])
+                label = label.split('_' + part[-1])[0]
                 if label not in labels_list:
                     categories_list.append(categories(label, labels_list))
                     labels_list.append(label)
@@ -135,11 +146,32 @@ def deal_json(img_path, json_path):
                 points = shapes['points']
                 p_type = shapes['shape_type']
                 if p_type == 'polygon':
-                    annotations_list.append(
-                        annotations_polygon(iscrowd, data['imageHeight'], data[
+                    if len(part[-1]) > 1:
+                        lmid = part[-1][1:]
+                        if lmid in lmid_count:
+                            real_count = lmid_count[lmid]
+                            real_anno = None
+                            for anno in annotations_list:
+                                if anno['id'] == real_count:
+                                    real_anno = anno
+                                    break
+                            annotations_polygon(anno, iscrowd, data['imageHeight'], data[
+                                'imageWidth'], points, label, num, label_to_num,
+                                                real_count)
+                            count -= 1
+                        else:
+                            lmid_count[lmid] = count
+                            anno = {}
+                            annotations_polygon(anno, iscrowd, data['imageHeight'], data[
+                                'imageWidth'], points, label, num, label_to_num,
+                                                count)
+                            annotations_list.append(anno)
+                    else:
+                        anno = {}
+                        annotations_polygon(anno, iscrowd, data['imageHeight'], data[
                             'imageWidth'], points, label, num, label_to_num,
-                                            count))
-
+                                            count)
+                        annotations_list.append(anno)
                 if p_type == 'rectangle':
                     points.append([points[0][0], points[1][1]])
                     points.append([points[1][0], points[0][1]])
@@ -180,14 +212,14 @@ def main():
     train_json_path = osp.join(
         res_dir + '/annotations',
         'instance_{}.json'.format(
-            os.path.basename(os.path.abspath(args.image_input_dir)), ), )
+            os.path.basename(os.path.abspath(args.image_input_dir))))
     json.dump(
         train_data_coco,
         open(
             train_json_path,
-            'w', ),
+            'w'),
         indent=4,
-        cls=MyEncoder, )
+        cls=MyEncoder)
 
 
 if __name__ == '__main__':
